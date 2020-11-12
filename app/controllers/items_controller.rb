7a17d88467b93f7d85cd61679aed5df5dfd070e8
Nathanstuff <n.odonnell@hotmail.com>
2020-11-13 02:05:42 +1100

    add set to items model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,7 +50,7 @@ class ItemsController < ApplicationController
   
       # Only allow a list of trusted parameters through.
       def item_params
-        params.require(:item).permit(:title, :description, :thumbnail, :price, :condition)
+        params.require(:item).permit(:title, :description, :thumbnail, :price, :condition, :set)
       end
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_144938) do
+ActiveRecord::Schema.define(version: 2020_11_12_145823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_11_12_144938) do
     t.decimal "price"
     t.string "thumbnail"
     t.string "condition"
+    t.string "set"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
