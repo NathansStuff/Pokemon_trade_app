a92e21f09bbd546da57c3bd63ffab4d21bcc4df5
Nathanstuff <n.odonnell@hotmail.com>
2020-11-16 15:28:46 +1100

    webhook added

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,4 +2,13 @@ class ChargesController < ApplicationController
       def success
         redirect_to items_path, notice: "You have successfully bought an item!"
       end
+
+      def webhook
+        payment_id= params[:data][:object][:payment_intent]
+        payment = Stripe::PaymentIntent.retrieve(payment_id)
+        item_id = payment.metadata.item_id
+
+        redirect_to items_path, notice: "You have successfully bought an item!"
+
+    end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'charges/success' => 'charges#success'
   get 'sellings/home' => 'sellings#home'
   get 'sellings/sold' => 'sellings#sold'
+  get "/payments/success", to: "payments#success"
+  post 'payments/webhook', to: 'payments#webhook'
   resources :conversations do
     resources :messages
   end
