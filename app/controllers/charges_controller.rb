dbeca2eb4deed22bfd31434654a5f575e176b97e
Nathanstuff <n.odonnell@hotmail.com>
2020-11-18 16:03:13 +1100

    more comments

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,7 +3,9 @@ class ChargesController < ApplicationController
 #-------------------------------------------------------------------------------
 # Upon successful payment, sets a notice and redirects the page to the items index page
 #-------------------------------------------------------------------------------  
-    def success
-        redirect_to items_path, notice: "You have successfully bought an item!"
-      end
+  def success
+      redirect_to items_path, notice: "You have successfully bought an item!"
+      @item=Item.find(params[:itemId])
+      @item.active = false
+  end
 end
